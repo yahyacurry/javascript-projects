cd5dc53a8c89508b3bc336026132f892cd062416
yahyacurry <reduxwolverine@gmail.com>
2024-04-04 13:11:23 -0400

    Changes

--- quiz-app/game.js
+++ quiz-app/game.js
@@ -12,39 +12,50 @@ let questionCounter = 0;
 let availableQuesions = [];
 
 let questions = [];
-
-fetch(
-    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
-)
+fetch('questions.json')
     .then((res) => {
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion;
-        });
-
+        questions = loadedQuestions;
         startGame();
     })
     .catch((err) => {
         console.error(err);
     });
+
+// fetch(
+//     'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+// )
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((loadedQuestions) => {
+//         questions = loadedQuestions.results.map((loadedQuestion) => {
+//             const formattedQuestion = {
+//                 question: loadedQuestion.question,
+//             };
+
+//             const answerChoices = [...loadedQuestion.incorrect_answers];
+//             formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+//             answerChoices.splice(
+//                 formattedQuestion.answer - 1,
+//                 0,
+//                 loadedQuestion.correct_answer
+//             );
+
+//             answerChoices.forEach((choice, index) => {
+//                 formattedQuestion['choice' + (index + 1)] = choice;
+//             });
+
+//             return formattedQuestion;
+//         });
+
+//         startGame();
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     });
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
