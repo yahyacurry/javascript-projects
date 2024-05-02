d4c55ca477a2de8b3e17752f9ba3873ec0b9de2d
yahyacurry <reduxwolverine@gmail.com>
2024-05-01 23:20:48 -0400

    Kahoot Working

--- Kahoot/CODE/Javascript/multiply.js
+++ Kahoot/CODE/Javascript/multiply.js
@@ -1,7 +1,9 @@
 const option1 = document.getElementById("option1"),
       option2 = document.getElementById("option2"),
       option3 = document.getElementById("option3"),
-      audio = document.getElementById("myAudio");  
+      audio = document.getElementById("myAudio"),
+      audio2 = document.getElementById("myAudio2");  
+
 var answer = 0;
 
 function generate_equation(){ 
@@ -31,6 +33,7 @@ function generate_equation(){
 
 option1.addEventListener("click", function(){
     if(option1.innerHTML == answer){
+      audio2.play();
       generate_equation();
     }
     else{
@@ -40,6 +43,7 @@ option1.addEventListener("click", function(){
 
 option2.addEventListener("click", function(){
     if(option2.innerHTML == answer){
+      audio2.play();
       generate_equation();
     }
     else{
@@ -49,6 +53,7 @@ option2.addEventListener("click", function(){
 
 option3.addEventListener("click", function(){
     if(option3.innerHTML == answer){
+      audio2.play();
      generate_equation();
     }
     else{
