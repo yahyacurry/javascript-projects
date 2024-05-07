dfdf82573b74b9937dc3da2f971c49daf2a3a3cc
yahyacurry <reduxwolverine@gmail.com>
2024-05-07 19:46:39 -0400

    Feel free to change the transition of the cube and hover effect

---
+++ 3d-Image-Cube-Slider/script.js
@@ -0,0 +1,15 @@
+let cube = document.querySelector(".image-cube")
+let btnPrev = document.getElementById("prev");
+let btnNext = document.getElementById("next");
+
+let pos = 0;
+
+btnNext.addEventListener("click", () =>{
+    pos -= 90;
+    cube.style.transform = `rotateY(${pos}deg)`
+} );
+
+btnPrev.addEventListener("click", () =>{
+    pos += 90;
+    cube.style.transform = `rotateY(${pos}deg)`
+} );
